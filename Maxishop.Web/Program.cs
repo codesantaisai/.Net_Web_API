0ce730f2bc5953f1c81e66e81529068088f592c8
codesantaisai <jathurshan@protonmail.com>
2024-01-18 10:12:00 -0800

    second commit of EF core Register

--- Maxishop.Web/Program.cs
+++ Maxishop.Web/Program.cs
@@ -1,7 +1,13 @@
+using Maxishop.Web.Data;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
+#region Database Connectivity
+builder.Services.AddDbContext<ApplicationDbContext>(Options => Options.UseSqlServer(builder.Configuration.GetConnectionString("ConnectionStrings")));
+#endregion
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
--- Maxishop.Web/obj/Debug/net8.0/Maxishop.Web.AssemblyInfo.cs
+++ Maxishop.Web/obj/Debug/net8.0/Maxishop.Web.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Maxishop.Web")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4a0f2175202bd7ca6e08c210447141d12ac96ce2")]
 [assembly: System.Reflection.AssemblyProductAttribute("Maxishop.Web")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Maxishop.Web")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
